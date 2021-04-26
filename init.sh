cb23572b8358ca1e6fbba45d3c321ca7c94062b6
liyuanz <l13925521794@163.com>
2021-04-26 15:12:43 +0800

    init

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# thinkphp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/lganame/thinkphp.git
+git push -u origin main
